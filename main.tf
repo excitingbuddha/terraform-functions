b1edf3d82a4a1826a4b84080171c53940b3f66ff
Michael Garrett <inuendo13@gmail.com>
2023-06-13 15:57:42 +0200

    Adding terraform functions

---
+++ main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.34.0"
+    }
+  }
+}
+
+resource "random_id" "bucket_prefix" {
+  byte_length = var.bucket_prefix_length
+}
+
+module "archive" {
+  source     = "git::https://github.com/excitingbuddha/terraform.git//modules/archive"
+  source_dir = var.source_dir
+}
+
+module "bucket" {
+  source               = "git::https://github.com/excitingbuddha/terraform.git//modules/bucket"
+  bucket_name          = "${random_id.bucket_prefix.hex}-gcf-source"
+  location             = var.location
+  bucket_prefix_length = var.bucket_prefix_length
+  project_id           = var.project_id
+  source_code          = module.archive.source
+  output_location      = module.archive.output_path
+}
+
+resource "google_service_account" "default" {
+  account_id   = var.service_account.account_id
+  display_name = var.service_account.display_name
+  project      = var.project_id
+}
+
+resource "google_cloudfunctions2_function" "default" {
+  name        = var.function_name
+  location    = var.region
+  description = var.description
+  project     = var.project_id
+
+  build_config {
+    runtime               = var.run_time
+    entry_point           = var.entry_point
+    environment_variables = {
+      BUILD_CONFIG_TEST = "build_test"
+    }
+    source {
+      storage_source {
+        bucket = module.bucket.bucket_name
+        object = module.bucket.bucket_object
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count    = var.max_instance
+    min_instance_count    = var.min_instance
+    available_memory      = var.available_memory
+    timeout_seconds       = var.timeout
+    environment_variables = {
+      SERVICE_CONFIG_TEST = "config_test"
+    }
+    vpc_connector                  = var.vpc_connector
+    ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email          = google_service_account.default.email
+  }
+}
+
+output "function_location" {
+  value       = google_cloudfunctions2_function.default.location
+  description = "Url of the cloudfunction"
+}
---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "project_id" {
+  description = "The ID of the Google Cloud project."
+  type        = string
+}
+
+variable "bucket_prefix_length" {
+  default = 10
+}
+
+variable "location" {
+  default = "eu"
+}
+
+variable "max_instance" {
+  default = 1
+}
+
+variable "timeout" {
+  default = 60
+}
+
+variable "function_name" {}
+
+variable "description" {
+  type = string
+}
+
+variable "min_instance" {
+  default = 1
+}
+
+variable "run_time" {
+  default = "go119"
+  type    = string
+}
+
+variable "available_memory" {
+  default = "128Mi"
+  type    = string
+}
+
+variable "entry_point" {
+  type = string
+}
+
+variable "region" {
+  description = "The region where the Cloud Function will be deployed."
+  type        = string
+}
+
+variable "service_account" {
+  type = object({
+    account_id   = string
+    display_name = string
+  })
+}
+
+variable "vpc_connector" {
+  description = "The name of the VPC connector to associate with the Cloud Function."
+  type        = string
+}
+
+variable "source_dir" {
+  description = "The directory containing the function source code."
+  type        = string
+}
+
+
+
