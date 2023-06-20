cb681d26d290075a576f413045de8944c5e881ce
Michael Garrett <inuendo13@gmail.com>
2023-06-20 11:06:17 +0200

    adding env variables

--- variables.tf
+++ variables.tf
@@ -66,11 +66,9 @@ variable "source_dir" {
 }
 
 variable "environment_variables" {
-  type        = map(string)
-  description = "Environment variables for the Cloud Run service"
-  default     = {
-    VARIABLE_NAME_1 = "value1"
-    VARIABLE_NAME_2 = "value2"
-    VARIABLE_NAME_3 = "value3"
-  }
+  type = map(object({
+    Key   = string
+    Value = string
+  }))
+  default = {}
 }
